e10374fc8bef53f4c4fe04bbc14bb3e84325516b
Amanda Tavares <amandatvt@hotmail.com>
2016-01-03 23:18:45 -0200

    Passado para php. Menu lateral dos conteudos nao ativos ainda

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
-
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -21,190 +19,62 @@
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-
 </head>
 
 <body>
-    <?php include "includes/includes.php";?>
-
-    <!-- Header Carousel -->
-    <header id="myCarousel" class="carousel slide">
-        <!-- Indicators -->
-        <ol class="carousel-indicators">
-            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-            <li data-target="#myCarousel" data-slide-to="1"></li>
-            <li data-target="#myCarousel" data-slide-to="2"></li>
-        </ol>
-
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner">
-            <div class="item active">
-                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide One');"></div>
-                <div class="carousel-caption">
-                    <h2>Caption 1</h2>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-                </div>
-            </div>
-            <div class="item">
-                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Two');"></div>
-                <div class="carousel-caption">
-                    <h2>Caption 2</h2>
-                </div>
-            </div>
-            <div class="item">
-                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Three');"></div>
-                <div class="carousel-caption">
-                    <h2>Caption 3</h2>
-                </div>
-            </div>
-        </div>
-
-        <!-- Controls -->
-        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-            <span class="icon-prev"></span>
-        </a>
-        <a class="right carousel-control" href="#myCarousel" data-slide="next">
-            <span class="icon-next"></span>
-        </a>
-    </header>
-
-    <!-- Page Content -->
-    <div class="container">
-
-        <!-- Marketing Icons Section -->
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header">
-                    Welcome to Modern Business
-                </h1>
-            </div>
-            <div class="col-md-4">
-                <div class="panel panel-primary">
-                    <div class="panel-heading">
-                        <h4><i class="fa fa-fw fa-question"></i> O que é biossegurança</h4>
-                    </div>
-                    <div class="panel-body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
-                        <a href="#" class="btn btn-default">Leia Mais</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="panel panel-primary">
-                    <div class="panel-heading">
-                        <h4><i class="fa fa-fw fa-flag"></i> Manuais de Biossegurança</h4>
-                    </div>
-                    <div class="panel-body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
-                        <a href="#" class="btn btn-default">Leia Mais</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="panel panel-primary">
-                    <div class="panel-heading">
-                        <h4><i class="fa fa-fw fa-youtube-play"></i> Mídia</h4>
-                    </div>
-                    <div class="panel-body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
-                        <a href="#" class="btn btn-default">Leia Mais</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- /.row -->
-
-        <!-- Portfolio Section -->
-        <div class="row">
-            <div class="col-lg-12">
-                <h2 class="page-header">Notícias</h2>
-            </div>
-            <div class="col-md-4 col-sm-6">
-                <a href="portfolio-item.html">
-                    <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
-                </a>
-            </div>
-            <div class="col-md-4 col-sm-6">
-                <a href="portfolio-item.html">
-                    <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
-                </a>
-            </div>
-            <div class="col-md-4 col-sm-6">
-                <a href="portfolio-item.html">
-                    <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
-                </a>
-            </div>
-            <div class="col-md-4 col-sm-6">
-                <a href="portfolio-item.html">
-                    <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
-                </a>
-            </div>
-            <div class="col-md-4 col-sm-6">
-                <a href="portfolio-item.html">
-                    <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
-                </a>
-            </div>
-            <div class="col-md-4 col-sm-6">
-                <a href="portfolio-item.html">
-                    <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
-                </a>
-            </div>
-        </div>
-
-    <!-- <hr>
-
-         Call to Action Section
-        <div class="well">
-            <div class="row">
-                <div class="col-md-8">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, expedita, saepe, vero rerum deleniti beatae veniam harum neque nemo praesentium cum alias asperiores commodi.</p>
-                </div>
-                <div class="col-md-4">
-                    <a class="btn btn-lg btn-default btn-block" href="#">Call to Action</a>
-                </div>
-            </div>
-        </div>
-
-        <hr> -->
-
-
-
+    <div id="nav">
+    <?php include "includes/nav.html";?>
     </div>
-    <!-- /.container -->
-<div id="footer"></div>
 
-    <!-- jQuery -->
+    <div id="content">
+    <?php
+    if(isset($_GET['pagina'])){
+    	//verificar o valor, caso seja um desconhecido nada acontece
+      //se for p1 vou incluir a pagina p1
+    	if($_GET['pagina']=="epi"){
+    		include "conteudo/epi.html";
+    	}else if($_GET['pagina']=="epc"){
+    		include "conteudo/epc.html";
+      }else if($_GET['pagina']=="pop"){
+        include "conteudo/pop.html";
+      }else if($_GET['pagina']=="fispq"){
+        include "conteudo/fispq.html";
+      }else if($_GET['pagina']=="bpl"){
+        include "conteudo/bpl.html";
+      }else if($_GET['pagina']=="incendios"){
+        include "conteudo/incendios.html";
+      }else if($_GET['pagina']=="mapa_de_risco"){
+        include "conteudo/mapa_de_risco.html";
+      }else if($_GET['pagina']=="manual"){
+        include "conteudo/manual/manual.html";
+
+      }else if($_GET['pagina']=="quimico"){
+        include "conteudo/quimico.html";
+      }else if($_GET['pagina']=="biologico"){
+        include "conteudo/biologico.html";
+      }else if($_GET['pagina']=="fisico"){
+        include "conteudo/fisico.html";
+      }else if($_GET['pagina']=="acidente"){
+        include "conteudo/acidente.html";
+      }else if($_GET['pagina']=="ergonomico"){
+        include "conteudo/ergonomico.html";
+      }else if($_GET['pagina']=="about"){
+        include "about.html";
+      }else if($_GET['pagina']=="contact"){
+        include "contact.html";
+      }
+    }else{
+    	include "inicio.html";
+    }
+    ?>
+    </div>
+
+    <div id="footer">
+    <?php include_once "includes/footer.html";?>
+    </div>
+
     <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
-
-    <!-- Script to Activate the Carousel -->
-    <script>
-    $('.carousel').carousel({
-        interval: 5000 //changes the speed
-    });
-
-    $(document).ready(function(){
-      $.ajax({
-        type:"get",
-        url: "nav.html",
-        success:function(a){
-          $('#nav').empty().append(a);
-        }
-      });
-      $.ajax({
-        type:"get",
-        url:"footer.html",
-        success:function(b){
-          $('#footer').empty().append(b);
-        }
-      });
-      })
-
-    </script>
 
 </body>
 
