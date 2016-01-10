c80c4da6ca2bb6adf5044407db70e7a3c212c3d4
Amanda Tavares <amandatvt@hotmail.com>
2016-01-09 23:05:09 -0300

    Novos conteudos adicionados.

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
     <link rel="icon" type="image/png" href="images/favicon.ico">
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/modern-business.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
